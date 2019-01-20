4f5441c01bd2122c97cf207a5584188b58966752
bhagyo <prachinmon1995@gmail.com>
2019-01-21 01:08:52 +0600

    changes the profile also

--- account/user_type/models.py
+++ account/user_type/models.py
@@ -1,29 +1,30 @@
 # eta all general_user er model
 
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
+from generaluser.models import User
 
 # A class for general user (doctor+patient)
 
 
 class Profile(models.Model):
     user = models.OneToOneField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=30, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
 
 
 class Doctor(models.Model):
     user = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    bmdc = models.IntegerField(null=False, blank=False)
+    bmdc = models.IntegerField(null=True, blank=True)
 
 
 class Patient(models.Model):
     user = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    disease = models.TextField()
+    disease = models.TextField(blank=True, null=True)
 
 
 '''
@@ -38,14 +39,18 @@ Doctor model ready hoy nai
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
+
+        print(instance)
+        print(kwargs)
         Profile.objects.create(user=instance)
         instance.profile.save()
-    if sender.doctor:
-        Doctor.objects.create(user=instance)
-        instance.profile.save()
-    else:
-        Patient.objects.create(user=instance)
-        instance.profile.save()
+
+        if instance.doctor:
+            Doctor.objects.create(user=instance)
+            instance.profile.save()
+        else:
+            Patient.objects.create(user=instance)
+            instance.profile.save()
 
 
 '''
